13c25cbe2070e98840a6898b35542df21a3bba10
Alekzander <bgnisaner@gmail.com>
2022-04-29 13:39:12 +0300

    Added some configs

--- motogram/motogram/settings.py
+++ motogram/motogram/settings.py
@@ -23,12 +23,15 @@ SECRET_KEY = 'django-insecure-0ok$54o#&!i1vool2=ss8mk#9i)=n#s!gf^l8a@!xcwmjz3%%w
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT')
+print(APP_ENVIRONMENT)
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'motogram-exam.herokuapp.com',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+# [
+#     'localhost',
+#     '127.0.0.1',
+#     'motogram-exam.herokuapp.com',
+# ]
 
 # Application definition
 DJANGO_APPS = (
@@ -85,16 +88,27 @@ WSGI_APPLICATION = 'motogram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd85lshc2q3uup2',
-        'USER': 'hqcbuacfxmrujb',
-        'PASSWORD': 'a7fb33fc0a9e628bbf0627a379b97b7252fd655e8f16b57155037e54af307ece',
-        'HOST': 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
+DATABASES = None
+if APP_ENVIRONMENT == 'Production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd85lshc2q3uup2',
+            'USER': 'hqcbuacfxmrujb',
+            'PASSWORD': 'a7fb33fc0a9e628bbf0627a379b97b7252fd655e8f16b57155037e54af307ece',
+            'HOST': 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    }
+
+print(DATABASES)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
